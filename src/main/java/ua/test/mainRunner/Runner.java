477ec54632834a677f15fdf47e2a2e7abc5e2c63
l3monxdd <keepers944@gmail.com>
2019-02-01 23:49:38 +0200

    added Car entity

--- src/main/java/ua/test/mainRunner/Runner.java
+++ src/main/java/ua/test/mainRunner/Runner.java
@@ -4,6 +4,7 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
 import org.hibernate.cfg.Configuration;
+import ua.test.model.Cars;
 import ua.test.model.User;
 
 public class Runner {
@@ -16,7 +17,11 @@ public class Runner {
 
         Runner runner = new Runner();
 
-        runner.addUser("Viktor");
+//        runner.addUser("Viktor");
+
+        runner.addCar("Audi","a4",2016,15000.0);
+
+
 
     }
 
@@ -36,5 +41,25 @@ public class Runner {
         session.close();
 
 
+    }
+
+    public void addCar(String brand,String model, int year, double price){
+        Session session = sessionFactory.openSession();
+
+        Transaction transaction = null;
+
+        transaction = session.beginTransaction();
+
+        Cars cars = new Cars();
+        cars.setBrand(brand);
+        cars.setModel(model);
+        cars.setYear(year);
+        cars.setPrice(price);
+
+        session.save(cars);
+
+        transaction.commit();
+
+        session.close();
     }
 }
